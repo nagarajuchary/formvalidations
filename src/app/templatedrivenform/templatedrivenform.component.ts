f44795d79e9e8f183a76372edba7c1094b0cb77e
nagarajuchary <nagarajuvita13@gmail.com>
2024-03-28 14:38:42 +0530

    methodsInTs

--- src/app/templatedrivenform/templatedrivenform.component.ts
+++ src/app/templatedrivenform/templatedrivenform.component.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-templatedrivenform',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterLink,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './templatedrivenform.component.html',
   styleUrl: './templatedrivenform.component.css'
 })
@@ -15,46 +15,46 @@ export class TemplatedrivenformComponent implements OnInit {
 
   constructor(private form: FormBuilder) {
 
-  
+
   }
   ngOnInit(): void {
-   this.regform=this.form.group({
-    fname:["",Validators.required,Validators.pattern("") ],
-    mobile:["", Validators.required],
-    email:["", Validators.required],
-    dob:["", Validators.required],
-    address:["", Validators.required],
-    password:["", Validators.required],
-    cbox:["", Validators.required]
+    this.regform = this.form.group({
+      fname: new FormControl("", [Validators.required, Validators.minLength(3), Validators.pattern("[A-Z].*")]),
+      mobile: new FormControl("", [Validators.required,Validators.maxLength(10), Validators.pattern("[0-9]*")]),
+      email: new FormControl("",[ Validators.required, Validators.email]),
+      dob: ["", Validators.required],
+      address: ["", Validators.required],
+      password: new FormControl("",[ Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$')]),
+      cbox: ["", Validators.required]
 
-   });
-   
+    });
+
   }
-  get Fname(){
-   return this.regform.get("fname") as FormControl
+  get Fname(): FormControl {
+    return this.regform.get("fname") as FormControl
   }
-  
-  get Mobile(){
+
+  get Mobile(): FormControl {
     return this.regform.get("mobile") as FormControl
   }
 
-  get Email(){
+  get Email(): FormControl {
     return this.regform.get("email") as FormControl
   }
-  
-  get Dob(){
+
+  get Dob() {
     return this.regform.get("dob") as FormControl
   }
 
-  get Address(){
+  get Address() {
     return this.regform.get("address") as FormControl
   }
 
-  get Password(){
+  get Password() {
     return this.regform.get("password") as FormControl
   }
 
-  get Cbox(){
+  get Cbox() {
     return this.regform.get("cbox") as FormControl
   }
 }
